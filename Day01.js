a4598a13e5a25acef3d8c9903a05263c2a6dbe7e
Shubham Patki <104133825+Shubhampatki20@users.noreply.github.com>
2022-12-12 19:49:18 +0530

    Add files via upload

---
+++ Day01.js
@@ -0,0 +1,8 @@
+console.log("Hello")
+
+let Name='Shubham';
+let LastName='Patki';
+const Mobilenumber=3322123456;
+console.log(Name);
+console.log(LastName);
+console.log(Mobilenumber);
---
+++ Javascript practice.js
@@ -0,0 +1,43 @@
+// 7
+// 2.5
+// // Both are treated as number 
+
+// ("Shubham")
+// ('Anurag')
+// // Both are String.
+
+// true
+// false
+// // False=0 and true=1 This are Bollean values
+
+// null
+// undefined
+// // Empty values
+
+// //All above called as primitive values
+
+// // Arrays and Object
+// [1,1.2,"Anurag"]
+// Index=0,1,2,3
+
+// // Object
+// // This is Key value pair
+
+// {
+//     Name:Shubham;
+//     LastName:Patki;
+// }
+
+
+// // Varibles : which hold value
+// // Type of varible let,const,
+
+// let Name='Shubham';
+// let LastName='Patki';
+// const Mobilenumber=3322123456;
+
+// console.log(Name);
+// console.log(LastName);
+// console.log(Mobilenumber);
+
+console.log("")
