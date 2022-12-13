f118543ac1db8da8ce390098595131b77bbb693e
Shubham Patki <104133825+Shubhampatki20@users.noreply.github.com>
2022-12-13 10:48:03 +0530

    Add files via upload

---
+++ Day02.js
@@ -0,0 +1,55 @@
+// Conditions 
+// if
+// Switch case
+// Ternary conditions
+
+// let x=10;
+// let y=40;
+// let z=x+y;
+
+// if(x<5) {
+//     console.log(z);
+// }else if(x==50){
+//     console.log("Good");
+// }
+// else{
+//     console.log("Not predictable")
+// }
+
+// Rating Mini app
+// let rating=50;
+
+// if (rating == 5){
+//     console.log("5 stars");
+// }else if(rating == 4){
+//     console.log("4 stars");
+// }else if(rating == 3){
+//     console.log("3 stars");
+// }else{
+//     console.log("Not elegible for rating");
+// }
+
+// Switch case
+// let user="admin"
+
+// switch (user){
+//     case "admin":
+//         console.log("Redirect Him/Her to the Admin Page");
+//         break;
+//     case "Mentor":
+//         console.log("Redirect Him/Her to the Mentor Dashboard");
+//         break;
+//     default:
+//     console.log("I am simple user")
+// }
+
+// Ternary operator/Ternary conditions
+// conditio?True:False
+
+// let raning = true;
+// raning ? console.log("raning"):console.log("Not raning")
+
+// data and Math
+
+const now = new Date();
+console.log(now)
