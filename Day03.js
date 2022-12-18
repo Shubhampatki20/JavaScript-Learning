4d324033e087585a5b6831519d05f5505d664383
Shubham Patki <104133825+Shubhampatki20@users.noreply.github.com>
2022-12-18 17:06:42 +0530

    Add files via upload

---
+++ Day03.js
@@ -0,0 +1,63 @@
+// console.log("Hello Harshad")
+
+// arrays method
+
+//  let names=['Hitesh','Anurag','Surya','Anirudh','Bipul'];
+//  console.log(names);
+//  console.log(names.length);
+//  console.log(names[3]);
+//  console.log(names[names.length - 1]);
+//  names[4]='Vinay';
+//  console.log(names);
+
+// push:insert new value in array
+
+// names.push('Vyom')
+// console.log(names)
+
+//slice:print only thos number whicj we want
+// console.log(names.slice(1,3));
+// console.log(names.splice(0,0))
+
+// Splice:insert value inside array 
+
+let names=['Hitesh','Anurag','Surya','Anirudh','Bipul'];
+
+// names.splice(1,1,'Kamesh','Dharmesh');
+// console.log(names);
+
+// Concatination
+// let arr1=[1,2,3];
+// let arr2=[1,2,3];
+// let arr3=[1,2,3];
+// console.log(arr1.concat(arr2));
+// console.log(arr1.concat(arr2,arr3));
+
+// // includes
+// let arr1=[1,2,3];
+// console.log(arr1.includes(2,1))
+
+// indexof
+// let arr1=[1,2,3,4,5]
+// console.log(arr1.indexOf(2))
+
+// isArray()
+// let arr1=[1,2,3,4];
+// console.log(Array.isArray(arr1))
+
+// Join
+// let arr1=[1,2,3,4];
+// console.log(arr1.join('and'))
+
+// lastindexof
+// let arr1=[1,2,3,4];
+// console.log(arr1.indexOf(3))
+
+// map
+let arr1=[1,2,3,4];
+console.log(arr1.map(Math.sqrt));
+
+
+
+
+
